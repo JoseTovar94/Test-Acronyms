49c0b7bb86e88f89ec95404dd0afbd6f768da0b6
Jose Luis Tovar Jimenez <jose.tovar@alphacredit.co>
2021-03-28 19:31:11 -0500

    test unit

--- AcronymsTests/AcronymsTests.swift
+++ AcronymsTests/AcronymsTests.swift
@@ -9,25 +9,62 @@ import XCTest
 @testable import Acronyms
 
 class AcronymsTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var viewControllerUnderTest: SearchViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Search", bundle: nil)
+        self.viewControllerUnderTest = storyboard.instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
+        
+        self.viewControllerUnderTest.loadView()
+        self.viewControllerUnderTest.viewDidLoad()
     }
-
-    override func tearDownWithError() throws {
+    
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testHasATableView() {
+        XCTAssertNotNil(viewControllerUnderTest.tableView)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    
+    func testTableViewHasDelegate() {
+        XCTAssertNotNil(viewControllerUnderTest.tableView.delegate)
     }
-
+    
+    func testHasASearchBar() {
+        XCTAssertNotNil(viewControllerUnderTest.searchBar)
+    }
+    
+    func testTableViewConfromsToTableViewDelegateProtocol() {
+        XCTAssertTrue(viewControllerUnderTest.conforms(to: UITableViewDelegate.self))
+        XCTAssertTrue(viewControllerUnderTest.responds(to: #selector(viewControllerUnderTest.tableView(_:didSelectRowAt:))))
+    }
+    
+    func testTableViewHasDataSource() {
+        XCTAssertNotNil(viewControllerUnderTest.tableView.dataSource)
+    }
+    
+    func testTableViewConformsToTableViewDataSourceProtocol() {
+        XCTAssertTrue(viewControllerUnderTest.conforms(to: UITableViewDataSource.self))
+        XCTAssertTrue(viewControllerUnderTest.responds(to: #selector(viewControllerUnderTest.numberOfSections(in:))))
+        XCTAssertTrue(viewControllerUnderTest.responds(to: #selector(viewControllerUnderTest.tableView(_:numberOfRowsInSection:))))
+        XCTAssertTrue(viewControllerUnderTest.responds(to: #selector(viewControllerUnderTest.tableView(_:cellForRowAt:))))
+    }
+    
+    // MARK: - SearchBar
+    
+    func testSUT_HasSearchBar() {
+        
+        XCTAssertNotNil(viewControllerUnderTest.searchBar)
+    }
+    
+    func testSUT_ShouldSetSearchBarDelegate() {
+        
+        XCTAssertNotNil(self.viewControllerUnderTest.searchBar.delegate)
+    }
 }
